9e08527099c7a80390ee368c9ed66e70a75def96
Ala Amancio <105252853+AlanaAmancio@users.noreply.github.com>
2023-12-27 16:40:35 -0300

    up

---
+++ index.js
@@ -0,0 +1,22 @@
+let resultado = calcularPartidas(193, 93)
+function calcularPartidas(vitorias, derrotas){
+    let saldo = vitorias - derrotas
+    if (saldo <= 10 ){
+console.log(`O herói tem de saldo de ${saldo} e está no nível ferro.`)
+    } else if (saldo >= 11 && saldo <= 20){
+        console.log(`O herói tem de saldo de ${saldo} e está no nível bronze.`)
+    } else if (saldo >= 21 && saldo <= 50){
+        console.log(`O herói tem de saldo de ${saldo} e está no nível prata.`)
+    } else if (saldo >= 51 && saldo <= 80){
+        console.log(`O herói tem de saldo de ${saldo} e está no nível ouro.`)
+    } else if (saldo >= 81 && saldo <= 90){
+        console.log(`O herói tem de saldo de ${saldo} e está no nível diamante.`)
+    } else if (saldo >= 91 && saldo <= 100){
+        console.log(`O herói tem de saldo de ${saldo} e está no nível lendário.`)
+    } else if (saldo >= 101){
+        console.log(`O herói tem de saldo de ${saldo} e está no nível imortal.`)
+
+    }
+
+    return saldo
+}
