74d27deb0cbb3f85f3a214531f76afc6d5479e84
Ala Amancio <105252853+AlanaAmancio@users.noreply.github.com>
2023-12-27 17:12:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-let resultado = calcularPartidas(193, 93)
-class heroi (vitorias, derrotas){
-    let saldo = vitorias - derrotas
-    if (saldo <= 10 ){
+class heroi{
+    constructor (nome, idade, tipo){
+    this.nome = nome
+    this.idade = idade
+    this.tipo = tipo
+}
+    atacar() {
+        let resultado = this.calculadoradepartidas(91, 32)
+
+        switch (saldo <= 10 ) {
 console.log(`O herói tem de saldo de ${saldo} e está no nível ferro.`)
-    } else if (saldo >= 11 && saldo <= 20){
+        }   case  (saldo >= 11 && saldo <= 20){
         console.log(`O herói tem de saldo de ${saldo} e está no nível bronze.`)
-    } else if (saldo >= 21 && saldo <= 50){
+        }   case: (saldo >= 21 && saldo <= 50){
         console.log(`O herói tem de saldo de ${saldo} e está no nível prata.`)
-    } else if (saldo >= 51 && saldo <= 80){
+        }  case: (saldo >= 51 && saldo <= 80){
         console.log(`O herói tem de saldo de ${saldo} e está no nível ouro.`)
-    } else if (saldo >= 81 && saldo <= 90){
+        }  case (saldo >= 81 && saldo <= 90){
         console.log(`O herói tem de saldo de ${saldo} e está no nível diamante.`)
-    } else if (saldo >= 91 && saldo <= 100){
+        }   case (saldo >= 91 && saldo <= 100){
         console.log(`O herói tem de saldo de ${saldo} e está no nível lendário.`)
-    } else if (saldo >= 101){
+        }   case (saldo >= 101){
         console.log(`O herói tem de saldo de ${saldo} e está no nível imortal.`)
 
-    }
-
+        }
+}
     return saldo
 }
